27f7975ed28345a22b044b8fe27eddd21d023398
guofuqiang00 <gfqjava@163.com>
2020-09-19 17:01:09 +0800

    init

---
+++ vue2-pro/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//
+import ElementUi from 'element-ui'
+Vue.use(ElementUi)
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
